53255ef3445cf5c8e00dbf0a6396408f7fe22536
Kat Bedell <kattrichard@gmail.com>
2020-08-24 21:07:31 -0400

    Added NPCs

---
+++ module/actor/sheets/npc.js
@@ -0,0 +1,66 @@
+import { ActorSheetQuest } from "./base.js";
+
+/**
+ * An Actor sheet for player character type actors in the Quest system.
+ * Extends the base ActorSheetQuest class.
+ * @type {ActorSheetQuest}
+ */
+export class NPCSheetQuest extends ActorSheetQuest {
+  /**
+   * Define default rendering options for the NPC sheet
+   * @return {Object}
+   */
+  static get defaultOptions() {
+    return mergeObject(super.defaultOptions, {
+      classes: ["quest", "sheet", "actor", "npc"],
+      width: 700,
+      height: 400,
+      resize: true,
+    });
+  }
+
+  /* -------------------------------------------- */
+  /*  Rendering                                   */
+  /* -------------------------------------------- */
+
+  /**
+   * Get the correct HTML template path to use for rendering this particular sheet
+   * @type {String}
+   */
+  get template() {
+    if (game.user.isGM && this.actor.owner) {
+      return "systems/quest/templates/actors/npc-sheet.html";
+    }
+  }
+
+  /* -------------------------------------------- */
+
+  /* -------------------------------------------- */
+  /*  Event Listeners and Handlers
+  /* -------------------------------------------- */
+
+  /**
+   * Activate event listeners using the prepared sheet HTML
+   * @param html {HTML}   The prepared HTML object ready to be rendered into the DOM
+   */
+  activateListeners(html) {
+    super.activateListeners(html);
+
+    if (!this.options.editable) return;
+    if (this.actor.owner) {
+      html.find(".roll-generic").click(this._rollGeneric.bind(this));
+    }
+  }
+
+  /* -------------------------------------------- */
+
+  /**
+   * Handle rolling a generic for the Character
+   * @param {MouseEvent} event    The originating click event
+   * @private
+   */
+  _rollGeneric(event) {
+    event.preventDefault();
+    return this.actor.rollGeneric({ event: event, actor: this.actor.data });
+  }
+}
